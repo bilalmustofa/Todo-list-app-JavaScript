8b614a1534cb6a1d315a5536086fb305e98935dc
bilal <bilalmustofa146@gmail.com>
2026-04-07 02:39:44 -0600

    added JS delete task

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // To Add task  
 const addBtn = document.querySelector('#addBtn');
 const taskInput = document.querySelector('#newTask input');
+
 addBtn.addEventListener('click', () => {
     // input Validation
     const taskInputValue = taskInput.value;
@@ -11,16 +12,27 @@ addBtn.addEventListener('click', () => {
         const taskListDiv = document.createElement('div');
         const taskList = document.querySelector('#taskList');
         taskListDiv.className = "single-task";
-        taskList.appendChild(taskListDiv);
         // create span tag in to 'taskListDiv' for task Name
         const taskNameSpan = document.createElement('span');
         taskNameSpan.id = "taskName"
         taskNameSpan.textContent = taskInputValue;
-        taskListDiv.appendChild(taskNameSpan);
         // create delete button tag in 'taskListDiv' 
         const deleteBtn = document.createElement('button');
-        taskNameSpan.className = "delete";
+        deleteBtn.className = "delete";
         deleteBtn.textContent = "❌";
+
+        // append elements
+        taskListDiv.appendChild(taskNameSpan);
+        taskList.appendChild(taskListDiv);
         taskListDiv.appendChild(deleteBtn);
+
+        taskInput.value = "";
+    }
+});
+
+// Delete The Task
+taskList.addEventListener('click', function(e) {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentNode.remove();
     }
 });
