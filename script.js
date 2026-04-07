6df4e58958934a0a25503eb6c814dfb92d880cac
bilal <bilalmustofa146@gmail.com>
2026-04-07 01:54:35 -0600

    added some css and JS task

--- script.js
+++ script.js
@@ -6,5 +6,21 @@ addBtn.addEventListener('click', () => {
     const taskInputValue = taskInput.value;
     if(taskInputValue.length === 0) {
         alert("Please First Enter a tasks!")
+    } else {
+        // create container Div in task list container
+        const taskListDiv = document.createElement('div');
+        const taskList = document.querySelector('#taskList');
+        taskListDiv.className = "single-task";
+        taskList.appendChild(taskListDiv);
+        // create span tag in to 'taskListDiv' for task Name
+        const taskNameSpan = document.createElement('span');
+        taskNameSpan.id = "taskName"
+        taskNameSpan.textContent = taskInputValue;
+        taskListDiv.appendChild(taskNameSpan);
+        // create delete button tag in 'taskListDiv' 
+        const deleteBtn = document.createElement('button');
+        taskNameSpan.className = "delete";
+        deleteBtn.textContent = "❌";
+        taskListDiv.appendChild(deleteBtn);
     }
 });
